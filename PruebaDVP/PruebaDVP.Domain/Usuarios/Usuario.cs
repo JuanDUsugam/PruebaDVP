83097cd47c65068d6b51fc85e168a7a050fe16c3
JuandUsugaM <davidusugam@gmail.com>
2023-11-23 15:25:29 -0500

    Feature: Crear Entidad Usuario

---
+++ PruebaDVP/PruebaDVP.Domain/Usuarios/Usuario.cs
@@ -0,0 +1,10 @@
+﻿using PruebaDVP.Domain.Common;
+
+namespace PruebaDVP.Domain.Usuarios
+{
+    public class Usuario :EntityBase<Guid>
+    {
+        public string? User { get; set; }
+        public string? Pass { get; set; }
+    }
+}
