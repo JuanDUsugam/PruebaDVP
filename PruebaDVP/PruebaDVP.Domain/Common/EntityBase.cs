d07abec03e50b12eb86ae6dfd72c512cc76bcf3b
JuandUsugaM <davidusugam@gmail.com>
2023-11-23 15:24:35 -0500

    Feature: Crear entidad base

---
+++ PruebaDVP/PruebaDVP.Domain/Common/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace PruebaDVP.Domain.Common
+{
+    public class EntityBase<T>
+    {
+        public T? Identificador { get; set; }
+        public DateTime FechaDeCreacion { get; set; }
+    }
+}
