81ff134bd66c4bbf95d93dccd8e3bf694303d30c
JuandUsugaM <davidusugam@gmail.com>
2023-11-23 15:25:03 -0500

    Feature: Crear entidad persona

---
+++ PruebaDVP/PruebaDVP.Domain/Personas/Persona.cs
@@ -0,0 +1,14 @@
+﻿using PruebaDVP.Domain.Common;
+
+namespace PruebaDVP.Domain.Personas
+{
+    public class Persona : EntityBase<Guid>
+    {
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+        public int NumeroDeIdentificacion { get; set; }
+        public string Email { get; set; }
+        public string? TipoDeIdentificacion { get; set; }
+
+    }
+}
