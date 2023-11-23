66b5b80e7cb40472b3b7ee2063f55889f12a13db
JuandUsugaM <davidusugam@gmail.com>
2023-11-23 15:53:17 -0500

    Feature: Create Dbcontext SqlServer

---
+++ PruebaDVP/PruebaDVP.Infra/Persistence/SQLSever/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaDVP.Domain.Personas;
+using PruebaDVP.Domain.Usuarios;
+
+namespace PruebaDVP.Infra.Persistence.SQLSever.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts)
+        {
+
+        }
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
