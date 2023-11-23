5ded2d8cbb58b05135da51d6494bdd9ba269d7db
JuandUsugaM <davidusugam@gmail.com>
2023-11-23 16:48:29 -0500

    Feature: Create UnhandledExceptionBehavior class

---
+++ PruebaDVP/PruebaDVP.Application/Behaviors/UnhandledExceptionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace PruebaDVP.Application.Behaviors
+{
+    public class UnhandledExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application Request: Sucedio una excepción para el request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
