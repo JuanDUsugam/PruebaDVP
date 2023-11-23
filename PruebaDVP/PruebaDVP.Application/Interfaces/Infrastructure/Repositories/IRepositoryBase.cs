d40f2150f0cf4453f5eb3d937a07e2f30d76f38a
JuandUsugaM <davidusugam@gmail.com>
2023-11-23 16:57:52 -0500

    Feature: Create Interface IRepositoryBase

---
+++ PruebaDVP/PruebaDVP.Application/Interfaces/Infrastructure/Repositories/IRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaDVP.Application.Interfaces.Infrastructure.Repositories
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity> DeleteAsync(TEntity entity);
+
+    }
+}
