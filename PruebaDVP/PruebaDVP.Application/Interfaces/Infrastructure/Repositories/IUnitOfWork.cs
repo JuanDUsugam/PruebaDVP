d0073e2b3af17418c7024e88699566a7d3df56af
JuandUsugaM <davidusugam@gmail.com>
2023-11-23 16:58:14 -0500

    Feature: Create Interface IUnitOfWork

---
+++ PruebaDVP/PruebaDVP.Application/Interfaces/Infrastructure/Repositories/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace PruebaDVP.Application.Interfaces.Infrastructure.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepositoryBase<TEntity> Repository<TEntity>() where TEntity : class;
+        Task<int> Complete();
+    }
+}
