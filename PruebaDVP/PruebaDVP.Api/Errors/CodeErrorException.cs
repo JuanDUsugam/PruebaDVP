e6b903a4e5dd116e97e823e262d3ccb4a980e348
JuanDUsugam <daviusugam@gmail.com>
2023-11-25 10:11:31 -0500

    Feature: layer interface User completed

---
+++ PruebaDVP/PruebaDVP.Api/Errors/CodeErrorException.cs
@@ -0,0 +1,12 @@
+﻿namespace PruebaDVP.Api.Errors
+{
+    public class CodeErrorException : CodeErrorResponse
+    {
+        public string? Details { get; set; }
+
+        public CodeErrorException(int statusCode, string? mensaje = null, string? details = null) : base(statusCode, mensaje)
+        {
+            Details = details;
+        }
+    }
+}
